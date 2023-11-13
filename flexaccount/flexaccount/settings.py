7e16f3a51a9d60dc24e0476f6028b22042928636
ratantiwaridev <brahmin1903@gmail.com>
2023-11-14 01:42:10 +0530

    media inserted

--- flexaccount/flexaccount/settings.py
+++ flexaccount/flexaccount/settings.py
@@ -132,5 +132,8 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR,'static'), 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static', 'media')
+MEDIA_DIRS = [
+    os.path.join(BASE_DIR, 'static/media'),
+]
